71bc94017de3e14b2804756fe1ea8fe129b5d221
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-11-26 16:02:54 -0800

    add single page

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import React from "react";
+import {
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardTitle,
+    Breadcrumb,
+    BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 // Renders a Single Item for the Directory
-function RenderDirectoryItem({ campsite, onClick }) {
+function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg
+                    width="100%"
+                    src={campsite.image}
+                    alt={campsite.name}
+                />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -23,6 +37,18 @@ function Directory(props) {
     });
     return (
         <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/home">Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
             <div className="row">{directory}</div>
         </div>
     );
