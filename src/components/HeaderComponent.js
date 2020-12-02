2b241767b0b128904032fc4babc4618696c6f457
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-12-02 01:20:35 -0800

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavbarToggler,
+    Collapse,
+    NavItem,
+    Jumbotron,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
     constructor(props) {
@@ -8,14 +24,31 @@ class Header extends Component {
 
         this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false,
         };
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
         this.setState({
-            isNavOpen: !this.state.isNavOpen
+            isNavOpen: !this.state.isNavOpen,
         });
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen,
+        });
+    }
+    handleLogin(event) {
+        alert(
+            `Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`
+        );
+        this.toggleModal();
+        event.preventDefault();
     }
     render() {
         return (
@@ -30,9 +63,16 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-                <Navbar dark sticky="top">
+                <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img
+                                src="/assets/images/logo.png"
+                                height="30"
+                                width="30"
+                                alt="NuCamp Logo"
+                            />
+                        </NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
@@ -42,8 +82,12 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/directory">
-                                        <i className="fa fa-list fa-lg" /> Directory
+                                    <NavLink
+                                        className="nav-link"
+                                        to="/directory"
+                                    >
+                                        <i className="fa fa-list fa-lg" />{" "}
+                                        Directory
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
@@ -52,14 +96,74 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <i className="fa fa-address-card fa-lg" /> Contact Us
+                                    <NavLink
+                                        className="nav-link"
+                                        to="/contactus"
+                                    >
+                                        <i className="fa fa-address-card fa-lg" />{" "}
+                                        Contact Us
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <span className="navbar-text ml-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in fa-lg" /> Login
+                                </Button>
+                            </span>
                         </Collapse>
                     </div>
                 </Navbar>
+                <Modal
+                    isOpen={this.state.isModalOpen}
+                    toggle={this.toggleModal}
+                >
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    innerRef={(input) =>
+                                        (this.username = input)
+                                    }
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    innerRef={(input) =>
+                                        (this.password = input)
+                                    }
+                                />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input
+                                        type="checkbox"
+                                        name="remember"
+                                        innerRef={(input) =>
+                                            (this.remember = input)
+                                        }
+                                    />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button
+                                type="submit"
+                                value="submit"
+                                color="primary"
+                            >
+                                Login
+                            </Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
