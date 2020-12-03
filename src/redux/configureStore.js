3246fa59d74099adcd7eabdc70efae1ef5a6d9c1
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-12-02 22:20:44 -0800

    React Redux Form Validation

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,12 @@ import { createStore } from "redux";
 import { Reducer, initialState } from "./reducer";
 
 export const ConfigureStore = () => {
-    const store = createStore(Reducer, initialState);
+    const store = createStore(
+        Reducer,
+        initialState,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
     return store;
 };
